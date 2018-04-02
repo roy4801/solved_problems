cd1f0f3ba2fb5a472a5b2789563740b21466adb3
roy4801 <a82611141@gmail.com>
2018-04-02 21:35:12 +0800

    uva p10633

---
+++ uva/10633.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+#define DEBUG 0
+
+int main()
+{
+	#if DEBUG
+	freopen("10633.in", "r", stdin);
+	#endif
+	
+	unsigned long long int input;
+	while(cin >> input && input != 0)
+	{
+		unsigned long long int prev = input / 9 * 10 + input % 9 - 1;
+		if(prev - prev / 10 == input)
+			cout << prev << ' ';
+		cout << input / 9 * 10 + input % 9 << endl;
+	}
+	return 0;
+}
