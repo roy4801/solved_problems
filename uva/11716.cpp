3665f2fdea6783690105f5ad748a840ee0b311af
roy4801 <a82611141@gmail.com>
2018-05-21 20:59:37 +0800

    10127 11716

---
+++ uva/11716.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <cstring>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	#ifndef ONLINE_JUDGE
+	freopen("./testdata/11716.in", "r", stdin);
+	freopen("./testdata/11716.out", "w", stdout);
+	#endif
+
+	int cases;
+	char str[10001]; // > 10000 => WA in C++
+
+	if(scanf("%d ", &cases) != EOF)
+	{
+		while(cases--)
+		{
+			fgets(str, 10000, stdin);
+
+			int length = strlen(str) - 1;
+			if(str[length] == '\n')
+			{
+				str[length] = '\0';
+			}
+			else
+				length++;
+
+			// Judge int
+			double rot = sqrt(length);
+			// if rot is integer
+			if(rot - (double)((int)rot) == 0.0)
+			{
+				int a = (int)rot;
+
+				for(int i = 0; i < rot; i++)
+				{
+					for(int j = 0; j < rot; j++)
+					{
+						printf("%c", str[i+j * a]);
+					}
+				}
+				putchar('\n');
+			}
+			else
+			{
+				printf("INVALID\n");
+			}
+		}
+	}
+	return 0;
+}
