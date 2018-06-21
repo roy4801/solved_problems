95ab43d779812f126f7494ad0b24af071908e8f9
roy4801 <a82611141@gmail.com>
2018-06-21 17:36:03 +0800

    10141

--- uva/10141.cpp
+++ uva/10141.cpp
@@ -1,4 +1,11 @@
+/*
+ * Uva 10141 - Request for Proposal
+ * author: roy4801
+ * AC(C++) 0.000
+ */
 #include <iostream>
+#include <cstring>
+#include <algorithm>
 
 using namespace std;
 
@@ -10,8 +17,21 @@ struct provider
 
 	provider()
 	{
+		name[0] = '\0';
 		price = 0.f;
 		i = 0;
+	}
+	provider(const provider &other)
+	{
+		strcpy(name, other.name);
+		price = other.price;
+		i = other.i;
+	}
+	provider& operator=(const provider& rhs)
+	{
+		strcpy(name, rhs.name);
+		price = rhs.price;
+		i = rhs.i;
 	}
 };
 
@@ -30,6 +50,27 @@ void skipInput(const int times)
 	}
 }
 
+struct compare
+{
+	bool operator()(const provider a, const provider b)
+	{
+		if(a.i > b.i)
+		{
+			return true;
+		}
+		else if(a.i == b.i)
+		{
+			if(a.price < b.price)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+}providerCompare;
+
+
+
 int main()
 {
 	#ifndef ONLINE_JUDGE
@@ -37,9 +78,20 @@ int main()
     freopen("./testdata/10141.out", "w", stdout);
     #endif
     int n, p;
+    int now = 1;
+    bool first = true;
 
     while(scanf("%d %d ", &n, &p) != EOF && n != 0 && p != 0)
     {
+    	if(first)
+    	{
+    		first = false;
+    	}
+    	else
+    	{
+    		putchar('\n');
+    	}
+
     	provider pro[p];
 
     	// Skip input
@@ -47,17 +99,22 @@ int main()
 
     	for(int i = 0; i < p; i++)
     	{
-    		scanf("%80s", pro[i].name);
+    		fgets(pro[i].name, 81, stdin);
+    		if(pro[i].name[strlen(pro[i].name)-1] == '\n')
+    			pro[i].name[strlen(pro[i].name)-1] = '\0';
+    		
     		scanf("%f %d ", &pro[i].price, &pro[i].i);
 
     		skipInput(pro[i].i);
     	}
 
-    	for(int i = 0; i < p; i++)
-    	{
-    		printf("%s %f %d\n", pro[i].name, pro[i].price, pro[i].i);
-    	}
-    	putchar('\n');
+    	sort(pro, pro + p, providerCompare);
+
+    	// for(int i = 0; i < p; i++)
+    	// {
+    	printf("RFP #%d\n", now++);
+		printf("%s\n", pro[0].name);
+    	// }
     }
 
     return 0;
