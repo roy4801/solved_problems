b2ad6ecbaa00ad50c7b76d4e158e8e931cb64ea9
roy4801 <a82611141@gmail.com>
2018-06-22 11:37:09 +0800

    10370

---
+++ uva/10370.cpp
@@ -0,0 +1,51 @@
+/*
+ * Uva 10370 - 	Above Average
+ * author: roy4801
+ * AC(C++) 0.000
+ */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	#ifndef ONLINE_JUDGE
+	freopen("./testdata/10370.in", "r", stdin);
+	freopen("./testdata/10370.out", "w", stdout);
+	#endif
+	int cases, people, tmp;
+	float avg, percent;
+
+	scanf("%d", &cases);
+
+	while(cases--)
+	{
+		scanf("%d", &people);
+
+		int grade[people];
+
+		avg = percent = 0.f;
+
+		for(int i = 0; i < people; i++)
+		{
+			scanf("%d", &grade[i]);
+
+			avg += (float)grade[i];
+		}
+
+		avg /= (float)people;
+
+		for(int i = 0; i < people; i++)
+		{
+			if((float)grade[i] > avg)
+			{
+				percent += 1.f;
+			}
+		}
+		percent /= (float)people;
+
+		printf("%.3f%%\n", percent * 100.f);
+	}
+
+	return 0;
+}
