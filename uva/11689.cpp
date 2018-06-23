36357abbe849b7a225e42cba5113a4cfa1dd0653
roy4801 <a82611141@gmail.com>
2018-06-23 14:13:59 +0800

    11689

---
+++ uva/11689.cpp
@@ -0,0 +1,41 @@
+/*
+ * Uva 11698 - Soda Surpler
+ * author: roy4801
+ * AC(C++) 0.000
+ */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	#ifndef ONLINE_JUDGE
+	freopen("./testdata/11689.in", "r", stdin);
+	freopen("./testdata/11689.out", "w", stdout);
+	#endif
+	int cases;
+	int junk, col, ex;
+	
+	scanf("%d", &cases);
+
+
+	while(scanf("%d %d %d", &junk, &col, & ex) != EOF
+		&& (junk || col || ex))
+	{
+		int drink = 0;
+		junk += col;
+
+		// keep exchanging the junk to soda and drink until not having enough junk
+		while(junk >= ex)
+		{
+			int buy = junk / ex;
+			junk -= buy * ex;
+			drink += buy;
+			junk += buy;
+		}
+
+		printf("%d\n", drink);
+	}
+
+	return 0;
+}
