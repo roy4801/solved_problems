5fc404a33a203cf21b415a11015df4a48307238f
roy4801 <a82611141@gmail.com>
2018-07-11 14:17:23 +0800

    11172

---
+++ uva/11172.cpp
@@ -0,0 +1,27 @@
+/*
+ * Uva 11172 - 
+ * author: roy4801
+ * AC(C++) 0.000
+ */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("./testdata/11172.in", "r", stdin);
+    freopen("./testdata/11172.out", "w", stdout);
+    #endif
+    int cases, a, b;
+
+    scanf("%d", &cases);
+
+    while(cases-- && scanf("%d %d", &a, &b) != EOF)
+    {
+        // The most hack code I have ever written
+        printf("%c\n", (a - b > 0 ? '>' : (a - b < 0 ?  '<' : '=') ));
+    }
+
+    return 0;
+}
