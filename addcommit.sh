3dbbe4f0b9ba011f3fe903ee356f29aac5ea07f4
roy4801 <a82611141@gmail.com>
2018-07-14 00:30:22 +0800

    shell script

---
+++ addcommit.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+
+git add uva/${1}.cpp uva/testdata/${1}.in uva/testdata/${1}.out
+git commit -m "${1}"
+git push origin master
---
+++ gen.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+
+touch uva/${1}.cpp
+touch uva/testdata/${1}.in
+touch uva/testdata/${1}.out
