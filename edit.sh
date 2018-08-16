a25a12ae1133a876e31a301ba0ee44a5b3684964
roy4801 <a82611141@gmail.com>
2018-08-16 16:39:01 +0800

    shell scripts

--- edit.sh
+++ edit.sh
@@ -1,3 +1,9 @@
-#!/bin/zsh
+#!/bin/bash
 
-open -a 'Sublime Text' uva/${1}.cpp uva/testdata/${1}.in uva/testdata/${1}.out
+platform=$(uname)
+
+if [[ "${platform}"=="Linux" ]]; then
+	subl uva/${1}.cpp uva/testdata/${1}.in uva/testdata/${1}.out
+elif [[ "${platform}"=="Darwin" ]]; then
+	open -a 'Sublime Text' uva/${1}.cpp uva/testdata/${1}.in uva/testdata/${1}.out
+fi
