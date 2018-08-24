3dc02a2be74a6aaa0f52b0ceae60ddaaa551430b
roy4801 <a82611141@gmail.com>
2018-08-24 14:40:37 +0800

    incomplete

--- uva/230.cpp
+++ uva/230.cpp
@@ -14,41 +14,41 @@ using namespace std;
 
 struct Book
 {
-	char title[81];
-	char cat;
-	bool borrow;
+	string title;
+	string author;
+	bool borrow, ret;
 
-	Book()
+	Book(): title(), author()
 	{
-		title[0] = '\0';
-		cat = 0;
-		borrow = false;
+		borrow = ret = false;
 	}
-	Book(const char t[], const char c, bool b = false)
+
+	Book(const char t[], const char a[], bool b = false, bool r = false) : title(t), author(a)
 	{
-		strcpy(title, t);
-		cat = c;
 		borrow = b;
+		ret = r;
 	}
 	//
 	Book(const Book &other)
 	{
-		strcpy(title, other.title);
-		cat = other.cat;
+		title = other.title;
+		author = other.author;
 		borrow = other.borrow;
+		ret = other.ret;
 	}
 
 	Book& operator=(const Book &other)
 	{
-		strcpy(title, other.title);
-		cat = other.cat;
+		title = other.title;
+		author = other.author;
 		borrow = other.borrow;
+		ret = other.ret;
 
 		return *this;
 	}
 	friend bool operator<(const Book &lhs, const Book &rhs)
 	{
-		return lhs.cat < rhs.cat ? (lhs.cat == rhs.cat && lhs.title[0] < rhs.title[0]) : false;
+		return lhs.author == rhs.author ? lhs.title < rhs.title : lhs.author < rhs.author;
 	}
 };
 
@@ -59,6 +59,18 @@ void skipUntil(const char c, const int times = 1)
 
 	while(t++ < times)
 		while((i = getchar()) != EOF && i != c);
+}
+
+void readUntil(const char c, char str[])
+{
+	char i;
+	int idx = 0;
+
+	while((i = getchar()) != EOF && i != c)
+	{
+		str[idx++] = i;
+	}
+	str[idx] = '\0';
 }
 
 int main()
@@ -74,26 +86,27 @@ int main()
 
 	while((c = getchar()) != EOF)
 	{
+		int tmpIdx = 0;
+
 		if(input)
 			switch(c)
 			{
 				case '"':
 				{
-					int tmpIdx = 0;
+					char author[81];
 
-					while((c = getchar()) && c != '"')
-					{
-						tmp[tmpIdx++] = c;
-					}
-					tmp[tmpIdx] = '\0';
+					readUntil('"', tmp);
 
 					skipUntil(' ', 2);
 
-					c = getchar();
+					tmpIdx = 0;
 
-					list.push_back(Book(tmp, c));
+					readUntil('\n', author);
 
-					skipUntil('\n');
+					// insert a book to the map
+					m[tmp] = list.size();
+					// push the book into the list
+					list.push_back(Book(tmp, author));
 				}
 				break;
 
@@ -107,26 +120,87 @@ int main()
 			switch(c)
 			{
 				case 'B':
+				{
 					skipUntil('"');
 
+					readUntil('"', tmp);
 
+					auto it = m.find(tmp);
+					if(it != m.end())
+					{
+						list[it->second].borrow = true;
+					}
+					skipUntil('\n');
+				}
 				break;
 
 				case 'R':
+				{
 					skipUntil('"');
+					readUntil('"', tmp);
 
+					auto it = m.find(tmp);
+					if(it != m.end())
+					{
+						list[it->second].ret = true;
+					}
+					skipUntil('\n');
+				}
+				break;
 
+				case 'S':
+				{
+					sort(list.begin(), list.end());
+					skipUntil('\n');
+					int b = 0;
+					bool first = false;
+
+					for(int i = 0; i < list.size(); i++)
+						if(list[i].borrow)
+							b++;
+					// number of borrowed books == number of all books
+					// means that the shelf is empty
+					if(b == list.size())
+						first = true;
+
+					int onShelfIdx = -1;
+					for(int i = 0; i < list.size(); i++)
+					{
+						// not borrowed
+						if(!list[i].borrow)
+						{
+							onShelfIdx = i;
+						}
+						else if(list[i].ret)
+						{
+							printf("Put ");
+							
+							if(onShelfIdx != -1)
+							{
+								printf("\"%s\" after \"%s\"\n", list[i].title.c_str(), list[onShelfIdx].title.c_str());
+							}
+							else
+							{
+								printf("\"%s\" first\n", list[i].title.c_str());
+							}
+							list[i].borrow = list[i].ret = false;
+							onShelfIdx = i;
+						}
+					}
+
+					printf("END\n");
+				}
 				break;
 			}
 		}
 	}
 
-	sort(list.begin(), list.end());
+	// sort(list.begin(), list.end());
 
-	for(auto &i : list)
-	{
-		printf("%s %c\n", i.title, i.cat);
-	}
+	// for(auto &i : list)
+	// {
+	// 	printf("%s %s %s %s\n", i.title.c_str(), i.author.c_str(), i.borrow ? "Borrowed" : "Not borrowed", i.ret ? "ret!" : "Not ret");
+	// }
 
 	return 0;
 }
