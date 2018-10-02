1c8d34be68895f9668822af0555cb53a7d5e8905
roy4801 <a82611141@gmail.com>
2018-10-02 09:56:11 +0800

    12461

---
+++ uva/12461.cpp
@@ -0,0 +1,24 @@
+/*
+ * Uva 12461 - Airplane
+ * author: roy4801
+ * (C++) 
+ */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	#ifndef ONLINE_JUDGE
+	freopen("./testdata/12461.in", "r", stdin);
+	freopen("./testdata/12461.out", "w", stdout);
+	#endif
+	int n;
+
+	while(scanf("%d", &n) != EOF && n)
+	{
+		printf("1/2\n");
+	}
+
+	return 0;
+}
