4a3391612f149fffc5fdc56c7064d8b71609bb7f
roy4801 <a82611141@gmail.com>
2018-11-03 16:42:50 +0800

    [script] addcommit.sh

--- addcommit.sh
+++ addcommit.sh
@@ -3,5 +3,5 @@
 # usage: ./addcommit.sh <type> <problem>
 
 git add ${1}/${2}.cpp ${1}/testdata/${2}.in ${1}/testdata/${2}.out
-git commit -m "${1}/${2}"
+git commit -m "[${1}] ${2}"
 git push origin master
