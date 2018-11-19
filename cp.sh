da601a74a749a5c8cb87ec8907cb07c4f712e2df
roy4801 <a82611141@gmail.com>
2018-11-19 23:18:56 +0800

    [srcipt] cp.sh

--- cp.sh
+++ cp.sh
@@ -4,11 +4,13 @@
 
 clear
 
+INCLUDE="-I."
+
 if ! [[ "$1" == "uva" ]]; then
 	FLAG=-DDBG
 fi
 
-g++ -std=c++11 ${FLAG} -Wall ${1}/${2}.cpp -o ${1}/a.out
+g++ -std=c++11 ${INCLUDE} ${FLAG} -Wall ${1}/${2}.cpp -o ${1}/a.out
 if [ "$?" == "0" ]; then
 	pushd ${1}/ > /dev/null
 	echo Running
