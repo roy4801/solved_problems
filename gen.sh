bfe3d6faef9465571dae8d90bd59a7d8457f1be4
roy4801 <a82611141@gmail.com>
2018-11-19 19:56:48 +0800

    [script] Be able to attach problem info to README.md

--- gen.sh
+++ gen.sh
@@ -10,6 +10,8 @@ else
 	cat template.cpp | sed -e "s/{judge}/${JUDGE}/g" -e "s/{pid}/${2}/g" -e "s/{pname}/${3}/g" -e "s/{DEFINE}/#ifdef DBG/g" > ${1}/${2}.cpp
 fi
 
+# echo >> ${1}/README.md
+echo "* [ ] ${2}" >> ${1}/README.md
 touch ${1}/testdata/${2}.in
 touch ${1}/testdata/${2}.out
 echo ${1}/${2} created.
