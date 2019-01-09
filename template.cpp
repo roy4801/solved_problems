c7832b9824d75c52a75094364b6bfccba4df64ec
roy4801 <a82611141@gmail.com>
2019-01-09 13:19:59 +0800

    [utils] template.cpp updated

--- template.cpp
+++ template.cpp
@@ -13,6 +13,12 @@ using namespace std;
 #define USE_CPPIO() ios_base::sync_with_stdio(0); cin.tie(0)
 typedef long long int LL;
 typedef unsigned long long ULL;
+typedef pair<int, int> P;
+#define F first
+#define S second
+#define INF 0x3f3f3f3f
+#define MP make_pair
+#define PB push_back
 
 int main()
 {
