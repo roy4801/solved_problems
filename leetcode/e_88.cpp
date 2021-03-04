d34a13ecbfbae0cbdbebbce13c164666f6c2f120
roy4801 <a82611141@gmail.com>
2021-03-05 00:35:46 +0800

    Add sol 2

--- leetcode/e_88.cpp
+++ leetcode/e_88.cpp
@@ -12,28 +12,52 @@ class Solution
 public:
     void merge(vector<int> &a, int m, vector<int> &b, int n)
     {
+        solve2(a, m, b, n);
+    }
+
+    void solve1(vector<int> &a, int m, vector<int> &b, int n)
+    {
         vector<int> ans;
         int i = 0, j = 0;
 
-        while(i < m && j < n)
+        while (i < m && j < n)
         {
-            if(a[i] > b[j])
+            if (a[i] > b[j])
                 ans.push_back(b[j]), j++;
             else
                 ans.push_back(a[i]), i++;
         }
         // only one while will be executed in the end
-        while(i < m)
+        while (i < m)
             ans.push_back(a[i]), i++;
-        while(j < n)
+        while (j < n)
             ans.push_back(b[j]), j++;
 
         print(ans);
+    }
+
+    // in-place
+    void solve2(vector<int> &a, int m, vector<int> &b, int n)
+    {
+        int i = m-1, j = n-1, k = m+n-1;
+        while (i >= 0 && j >= 0)
+        {
+            if (a[i] > b[j])
+                a[k] = a[i], i--;
+            else
+                a[k] = b[j], j--;
+            k--;
+        }
+        while (i >= 0)
+            a[k] = a[i], i--, k--;
+        while (j >= 0)
+            a[k] = b[j], j--, k--;
     }
 };
 
 int main()
 {
     vector<int> a{1,2,3,0,0,0}, b{2,5,6};
-    Solution{}.merge(a, 3, b, 2);
+    Solution{}.merge(a, 3, b, 3);
+    print(a);
 }
