7569460c2b6b95170fe63f025cfa9be1c761bb9f
roy4801 <a82611141@gmail.com>
2021-05-29 20:00:43 +0800

    Add support for std::map

--- leetcode/helper.h
+++ leetcode/helper.h
@@ -1,9 +1,26 @@
 #include <bits/stdc++.h>
 
 template<typename T>
-void print(const std::vector<T> &v)
+void print(const char *name, const std::vector<T> &v)
 {
-    for(auto &i : v)
-        cout << i << ' ';
-    cout << '\n';
+    cout << name << " = [";
+    for(int i = 0; i < v.size(); i++)
+        cout << (i > 0 ? ", " : "") << v[i];
+    cout << "]\n";
 }
+
+template<typename T1, typename T2>
+void print(const char *name, const std::map<T1, T2> &m)
+{
+    cout << name << " = {\n";
+    int n = 0;
+    for(auto && [k, v] : m)
+    {
+        cout << "  ";
+        cout << k << ": " << v << (n==m.size()-1 ? "" : ",") << '\n';
+        n++;
+    }
+    cout << "}\n";
+}
+
+#define DBG(x) print(#x, (x))
