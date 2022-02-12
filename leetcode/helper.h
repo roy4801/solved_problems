136eba727dd5373059c6e57064325b646e9b4d61
roy4801 <a82611141@gmail.com>
2022-02-12 18:25:39 +0800

    common: update helper

--- leetcode/helper.h
+++ leetcode/helper.h
@@ -5,42 +5,79 @@
 #include <bits/stdc++.h>
 
 template<typename T>
-void print(const char *name, const std::vector<T> &v)
+std::string plain_print(const T &v)
 {
-    cout << name << " = [";
-    for(int i = 0; i < v.size(); i++)
-        cout << (i > 0 ? ", " : "") << v[i];
-    cout << "]\n";
+    return std::to_string(v);
 }
 
-template<typename T1, typename T2>
-void print(const char *name, const std::map<T1, T2> &m)
+template<>
+std::string plain_print(const std::string &v)
 {
-    cout << name << " = {\n";
-    int n = 0;
-    for(auto && [k, v] : m)
-    {
-        cout << "  ";
-        cout << k << ": " << v << (n==m.size()-1 ? "" : ",") << '\n';
-        n++;
-    }
-    cout << "}\n";
+    return "\"" + v + "\"";
 }
 
 template<typename T>
-void print(const char *name, const std::set<T> &v)
+std::string plain_print(const std::vector<T> &v)
 {
-    cout << name << " = {";
-    int i = 0;
-    for(auto & a : v)
-        cout << (i++ > 0 ? ", " : "") << a;
-    cout << "}\n";
+    std::string res = "[ ";
+    int cnt = 0;
+
+    for(auto &i : v)
+    {
+        cnt++;
+        res += plain_print(i) + (cnt != v.size() ? ", " : "");
+    }
+
+    res += " ]";
+    return res;
+}
+
+template<typename T1, typename T2>
+std::string plain_print(const std::map<T1, T2> &m)
+{
+    std::string res = "{\n";
+    int n = 0;
+    for(auto && [k, v] : m)
+        res +=  "  " + plain_print(k) + " -> " + plain_print(v) + (n++==m.size()-1 ? "" : ",") + '\n';
+    res += "}\n";
+
+    return res;
+}
+
+template<typename T1, typename T2>
+std::string plain_print(const std::unordered_map<T1, T2> &m)
+{
+    std::string res = "{\n";
+    int n = 0;
+
+    std::map<T1, T2> tmp;
+    for(auto && [k, v] : m)
+        tmp[k] = v;
+
+    for(auto && [k, v] : tmp)
+        res +=  "  " + plain_print(k) + " -> " + plain_print(v) + (n++==m.size()-1 ? "" : ",") + '\n';
+    res += "}\n";
+
+    return res;
+}
+
+template<typename T>
+std::string plain_print(const std::set<T> &v)
+{
+    std::string res = "{";
+    int cnt = 0;
+
+    for(auto & i : v)
+        res += (cnt++ > 0 ? ", " : "") + plain_print(i);
+    res += "}\n";
+
+    return res;
 }
 
 template<typename T>
 void print(const char *name, const T &v)
 {
-    cout << name << " = " << v << '\n';
+    cout << name << " = " << plain_print(v) << '\n';
 }
 
 #define DBG(x) print(#x, (x))
