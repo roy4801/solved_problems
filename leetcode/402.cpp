f4000db2657440971cd07e138113c098449d2eed
roy4801 <a82611141@gmail.com>
2022-02-19 02:18:12 +0800

    fix comment

--- leetcode/402.cpp
+++ leetcode/402.cpp
@@ -24,7 +24,7 @@ public:
             
             ans.push_back(c);
         }
-        // if the nums are monotone ascending, delete digits from back
+        // if remaining nums are monotone ascending, delete digits from back
         while(k--)
             ans.pop_back();
         
