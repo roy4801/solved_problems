e74b91fa1fa6b7aaa0f76e4754b30731f141a529
roy4801 <a82611141@gmail.com>
2022-02-19 23:34:23 +0800

    leetcode: 1675

---
+++ leetcode/1675.cpp
@@ -0,0 +1,54 @@
+/*
+ * Leetcode Medium 1675. Magnetic Force Between Two Balls
+ * author: roy4801
+ * (C++)
+ */
+#include <bits/stdc++.h>
+using namespace std;
+#include "helper.h"
+
+class Solution
+{
+public:
+    int minimumDeviation(vector<int>& n)
+    {
+        priority_queue<int> pq;
+        int minN = INT_MAX, dif = INT_MAX;
+
+        for(int i : n)
+        {
+            if(i % 2 != 0)
+                i *= 2;
+            // printf("%d ", i);
+            pq.push(i);
+            minN = min(minN, i);
+        }
+        // puts("");
+
+
+
+        while(!pq.empty())
+        {
+            // printf("top = %d\n", pq.top());
+            if(pq.top() % 2 != 0)
+            {
+                dif = min(dif, pq.top() - minN);    
+                break;
+            }
+
+            dif = min(dif, pq.top() - minN);
+            minN = min(minN, pq.top() / 2);
+            pq.push(pq.top() / 2);
+            pq.pop(); // pop max
+        }
+
+        return dif;
+    }
+};
+
+int main()
+{
+    // vector<int> v = {1,2,3,4};
+    vector<int> v = {4,1,5,20,3};
+    cout << Solution{}.minimumDeviation(v) << '\n';
+}
