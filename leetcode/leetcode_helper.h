f47ef8533cd78a6adbb1e17f4f04a3e304d131a5
roy4801 <a82611141@gmail.com>
2022-02-23 21:52:21 +0800

    update helper

--- leetcode/leetcode_helper.h
+++ leetcode/leetcode_helper.h
@@ -60,4 +60,22 @@ ListNode* build_sll(const std::vector<int> &v)
     return head;
 }
 
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+
 #endif
