566116d753d6f4c6765836c5826999dd4b381590
roy4801 <a82611141@gmail.com>
2022-03-09 02:49:33 +0800

    leetcode: 141

---
+++ leetcode/141.cpp
@@ -0,0 +1,83 @@
+/*
+ * Leetcode Easy 141. Linked List Cycle
+ * author: roy4801
+ * AC(C++)
+ */
+#include <bits/stdc++.h>
+using namespace std;
+#include "helper.h"
+
+typedef pair<int, int> P;
+typedef long long int LL;
+#define PB push_back
+#define MP make_pair
+#define X first
+#define Y second
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution
+{
+public:
+    set<ListNode*> s;
+    bool intuition(ListNode *head)
+    {
+        ListNode *cur = head;
+        while(cur)
+        {
+            if(s.count(cur))
+            {
+                return true;
+            }
+            s.insert(cur);
+            cur = cur->next;
+        }
+        return false;
+    }
+
+    bool floyd(ListNode *head)
+    {
+        ListNode *a = head, *b = head;
+        while(b && b->next)
+        {
+            a = a->next;
+            b = b->next->next;
+            
+            if(a == b)
+                return true;
+        }
+        return false;
+    }
+
+    bool hasCycle(ListNode *head)
+    {
+        return floyd(head);
+    }
+};
+
+int main()
+{
+    // vector<int> v = {3,2,0,-4};
+    // int pos = 1;
+    vector<int> v = {1,2};
+    int pos = 0;
+    // vector<int> v = {1};
+    // int pos = -1;
+    //
+    ListNode *h = build_sll(v), *last = h, *c = h;
+    while(last->next)
+        last = last->next;
+    while(pos > 0 && c->next)
+    {
+        c = c->next;
+        pos--;
+    }
+    //
+    cout << Solution{}.hasCycle(h) << '\n';
+}
