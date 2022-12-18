5aa87c835925046be85773fe8e0f6ec9a10911cc
roy4801 <a82611141@gmail.com>
2022-12-18 21:34:58 +0800

    leetcode: 739

---
+++ leetcode/739.cpp
@@ -0,0 +1,44 @@
+/*
+ * Leetcode Medium 739. Daily Temperatures
+ * author: roy4801
+ * AC(C++)
+ */
+#include <bits/stdc++.h>
+using namespace std;
+#include "helper.h"
+
+typedef pair<int, int> P;
+typedef long long int LL;
+#define PB push_back
+#define MP make_pair
+#define X first
+#define Y second
+#define F first
+#define S second
+
+class Solution {
+public:
+    stack<pair<int, int>> s; // idx, temp
+    vector<int> ans;
+    vector<int> dailyTemperatures(vector<int>& temp)
+    {
+        ans.resize(temp.size(), 0);
+        s.push(make_pair(0, temp[0]));
+        for(int i = 0; i < temp.size(); i++)
+        {
+            while(!s.empty() && s.top().S < temp[i])
+            {
+                ans[s.top().F] = i - s.top().F;
+                s.pop();
+            }
+            s.push(make_pair(i, temp[i]));
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    vector<int> v = {73,74,75,71,69,72,76,73};
+    DBG(Solution{}.dailyTemperatures(v));
+}
