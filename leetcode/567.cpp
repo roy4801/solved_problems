d9c11a2f3709b5903c2ca3f076ebbbcfcc17d6e4
roy4801 <a82611141@gmail.com>
2023-02-05 04:21:10 +0800

    567

--- leetcode/567.cpp
+++ leetcode/567.cpp
@@ -7,6 +7,9 @@
 using namespace std;
 #include "helper.h"
 
+#define X first
+#define Y second
+
 class Solution
 {
 public:
@@ -74,7 +77,89 @@ public:
     }
 };
 
+class Solution3 {
+public:
+    int A[26], B[26];
+    bool checkInclusion(string a, string b) {
+        int n = a.size(), m = b.size();
+        if(n > m) return false;
+        for(int i = 0; i < n; i++)
+        {
+            A[a[i]-'a']++;
+            B[b[i]-'a']++;
+        }
+
+        for(int i = 0; i < m - n; i++)
+        {
+            if(equal(A, A+26, B))
+                return true;
+
+            B[b[n+i] - 'a']++;
+            B[b[i] - 'a']--;
+        }
+
+        return equal(A, A+26, B);
+    }
+};
+
+class Solution4 {
+public:
+    int A[26], B[26];
+    bool checkInclusion(string a, string b) {
+        int n = a.size(), m = b.size();
+        if(n > m) return false;
+        for(int i = 0; i < n; i++)
+        {
+            A[a[i]-'a']++;
+            B[b[i]-'a']++;
+        }
+
+        // 計算 a 和 b 在 [0,n) 有幾種字元個數相同
+        int cnt = 0;
+        for(int i = 0; i < 26; i++)
+            if(A[i] == B[i])
+                cnt++;
+
+        // 往右滑動窗口
+        for(int i = 0; i < m - n; i++)
+        {
+            if(cnt == 26)
+                return true;
+
+            int l = b[i]-'a', r = b[n+i]-'a'; // left char to idx, right char to idx
+
+            // 字串b 增加 r 字元 (右界往右移動一格)
+            B[r]++;
+            // 如果加入 r 之後，"字串b中 r 的個數"等於"字串a中 r 的個數"，則代表相等的數量增加 1，因為多了一種相等的字元種類 (r)
+            if(B[r] == A[r])
+                cnt++;
+            // 如果加入 r 之後比原來 (A[r]) 的多 1 ，則代表相等的數量減去 1，因為兩個字串 r 的各數不一樣
+            else if(B[r] == A[r] + 1)
+                cnt--;
+
+            // 字串b 刪除 l 字元 (左界往右移動一格)
+            B[l]--;
+            // 如果移除 l 之後，"字串b中 l 的個數"等於"字串a中 l 的個數"，則代表相等的數量增加 1，因為多了一種相等的字元種類 (l)
+            if(B[l] == A[l])
+                cnt++;
+            // 如果移除 l 之後比原來 (A[l]) 的少 1 ，則代表相等的數量減去 1，因為兩個字串 l 的各數不一樣                
+            else if(B[l] == A[l] - 1)
+                cnt--;
+        }
+
+        return cnt == 26;
+    }
+};
+
 int main()
 {
-    cout << Solution{}.checkInclusion("ab", "eidbaooo") << '\n';
+    // cout << Solution3{}.checkInclusion("ab", "eidbaooo") << '\n';
+    vector<pair<string,string>> in = {
+        {"ab", "eidbaooo"},
+        {"ab", "b"},
+    };
+    for(auto &i : in)
+    {
+        DBG(Solution4{}.checkInclusion(i.X, i.Y));
+    }
 }
