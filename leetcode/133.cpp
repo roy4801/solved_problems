68ac3066ffc338b7b63f7e0873fab28936f87319
roy4801 <a82611141@gmail.com>
2023-04-08 18:29:17 +0800

    leetcode: 133
    second try

--- leetcode/133.cpp
+++ leetcode/133.cpp
@@ -51,6 +51,84 @@ public:
     }
 };
 
+class Solution2 {
+public:
+    unordered_map<Node*, Node*> m; // old -> new
+    unordered_map<Node*, bool> visit;
+
+    inline Node* GetNode(Node *n)
+    {
+        if(!m.count(n))
+        {
+            m[n] = new Node();
+        }
+        return m[n];
+    }
+
+    Node* cloneGraph(Node* n)
+    {
+        if(!n)
+            return nullptr;
+
+        Node *ans;
+        queue<Node*> q;
+
+        q.push(n);
+        visit[n] = true;
+
+        while(!q.empty())
+        {
+            Node *cur = q.front(); q.pop();
+            GetNode(cur)->val = cur->val;
+
+            for(Node *i : cur->neighbors)
+            {
+                GetNode(i)->val = i->val;
+                GetNode(cur)->neighbors.push_back(GetNode(i));
+                
+                if(!visit[i])
+                {
+                    visit[i] = true;
+                    q.push(i);
+                }
+            }
+        }
+
+        return GetNode(n);
+    }
+};
+
+// dfs
+class Solution3 {
+public:
+    unordered_map<Node*, Node*> m; // old -> new
+    unordered_map<Node*, bool> visit;
+
+    inline Node* GetNode(Node *n)
+    {
+        if(!m.count(n))
+        {
+            m[n] = new Node();
+        }
+        return m[n];
+    }
+
+    Node* cloneGraph(Node* n)
+    {
+        if(!n) return nullptr;
+        if(m.count(n)) return m[n];
+
+        Node *nn = GetNode(n);
+        nn->val = n->val;
+        for(auto i : n->neighbors)
+        {
+            auto ii = cloneGraph(i);
+            nn->neighbors.push_back(ii);
+        }
+        return nn;
+    }
+};
+
 int main()
 {
     // im too lazy ...
