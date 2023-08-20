6ec43bfcf4c65fb79e8ffeaa8f9f43de906c9a5c
roy4801 <a82611141@gmail.com>
2023-08-20 21:29:17 +0800

    leetcode: 207 II

--- leetcode/207.cpp
+++ leetcode/207.cpp
@@ -15,6 +15,7 @@ typedef long long int LL;
 #define X first
 #define Y second
 
+// dfs coloring
 class Solution {
 public:
     bool canFinish(int n, vector<vector<int>>& preq)
@@ -51,6 +52,48 @@ public:
             if(color[i] == 0 && dfs(i))
                 return 0;
         return 1;
+    }
+};
+
+// topological sort
+class Solution2 {
+public:
+    bool canFinish(int n, vector<vector<int>>& preq)
+    {
+        // 建圖
+        vector<vector<int>> G(n);
+        for(auto &i : preq)
+        {
+            int a = i[0], b = i[1];
+            G[a].push_back(b);
+        }
+        // 統計每個點的入度
+        vector<int> conn(n); // in-degree
+        for(int i = 0; i < n; i++)
+            for(auto j : G[i])
+                conn[j]++;
+        // 找起始點 (入度=0的)，蒐集到 q 中
+        queue<int> q;
+        for(int i = 0; i < n; i++)
+            if(conn[i] == 0)
+                q.push(i);
+        // 將 q 裡的點拔掉
+        vector<int> topo;
+        while(!q.empty())
+        {
+            int cur = q.front();
+            q.pop();
+            topo.push_back(cur);
+            // 把 cur 連出去的邊都拔掉
+            for(int j : G[cur])
+            {
+                conn[j]--;
+                if(conn[j] == 0)
+                    q.push(j);
+            }
+        }
+        // 如果「拓撲排序的點數」不等於「圖的點數」，代表無法排出拓撲排序(有環)
+        return topo.size() == n;
     }
 };
 
