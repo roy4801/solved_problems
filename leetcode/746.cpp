56d6a5ff9a41bb4818432a18fcf92867c041615f
roy4801 <a82611141@gmail.com>
2023-10-14 05:37:00 +0800

    leetcode: 746
    speed up

--- leetcode/746.cpp
+++ leetcode/746.cpp
@@ -17,25 +17,21 @@ typedef long long int LL;
 
 class Solution {
 public:
-    int n;
-    vector<int> cost;
-    vector<int> dp;
-
     int minCostClimbingStairs(vector<int>& c)
     {
-        n = c.size();
-        cost = {0, 0};
+        vector<int> cost = {0, 0};
         cost.insert(cost.begin()+1, c.begin(), c.end());
-        dp.resize(cost.size()+5);
+        int n = c.size();
+        vector<int> dp(cost.size()+5);
 
         for(int i = 1; i <= n+1; i++)
         {
             if(i-2 >= 0)
-                dp[i] += min(dp[i-1]+cost[i], dp[i-2]+cost[i]);
-            else if(i-1 >= 0)
-                dp[i] += dp[i-1]+cost[i];
+                dp[i] += min(dp[i-1], dp[i-2])+cost[i];
+            else
+                dp[i] += cost[i];
         }
-
+        
         return dp[n+1];
     }
 };
