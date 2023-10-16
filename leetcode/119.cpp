a0541ca626bf85a0e501be59632968992d60686d
roy4801 <a82611141@gmail.com>
2023-10-16 09:20:33 +0800

    leetcode: 119
    second

--- leetcode/119.cpp
+++ leetcode/119.cpp
@@ -36,6 +36,26 @@ public:
     }
 };
 
+class Solution2 {
+public:
+    vector<int> getRow(int n)
+    {
+        vector<int> p[2] = {{1}, {1}};
+
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = 1; j < i; j++)
+            {
+                p[1][j] = p[0][j]+p[0][j-1];
+            }
+            p[1].push_back(1);
+            p[0] = p[1];
+        }
+
+        return p[1];
+    }
+};
+
 int main()
 {
     // skip
