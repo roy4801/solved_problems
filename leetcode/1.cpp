853a1c81658fa15c9714cab97cab1cb36f72387c
roy4801 <a82611141@gmail.com>
2025-06-30 09:41:49 +0800

    leetcode: 1

--- leetcode/1.cpp
+++ leetcode/1.cpp
@@ -49,6 +49,64 @@ end:
     }
 };
 
+// AC 9ms 16.5MB
+using P = pair<int,int>;
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target)
+    {
+        vector<int> ans;
+        int n = nums.size();
+        
+        auto comp = [](const P& a, const P& b) { return a.first < b.first; };
+        multiset<P, decltype(comp)> ns(comp); // num, idx
+        for(int i = 0; i < n; i++)
+            ns.emplace(nums[i], i);
+        
+        for(int i = 0; i < n; i++)
+        {
+            auto [a, b] = ns.equal_range({target - nums[i], -1});
+            if(a != ns.end())
+            {
+                ans.push_back(i);
+                for (auto it = a; it != b; it++)
+                {
+                    if(it->second != i)
+                    {
+                        ans.push_back(it->second);
+                        return ans;
+                    }
+                }
+            }
+            ans.clear();
+        }
+
+        return ans;
+    }
+};
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target)
+    {
+        vector<int> ans;
+        int n = nums.size();
+        
+        unordered_map<int,int> numToIdx; // num -> index
+        for(int i = 0; i < n; i++)
+        {
+            int c = target - nums[i];
+            if(numToIdx.count(c))
+            {
+                return {i, numToIdx[c]};
+            }
+            numToIdx[nums[i]] = i;
+        }
+
+        return ans;
+    }
+};
+
 int main()
 {
     vector<int> v = {2,7,11,15};
